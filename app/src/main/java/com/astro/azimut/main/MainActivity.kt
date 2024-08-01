5ee5d006d2ec8d38672613c00b5bd83a17e8f866
Aneod <blysboutique@gmail.com>
2024-08-01 11:37:21 +0200

    First map added.

--- app/src/main/java/com/astro/azimut/main/MainActivity.kt
+++ app/src/main/java/com/astro/azimut/main/MainActivity.kt
@@ -132,7 +132,6 @@ fun MapScreen(onMapViewInitialized: (MapView) -> Unit) {
         .position(montparnasseTowerCoordinates)
         .title("Marker")
 
-
     Box(modifier = Modifier.fillMaxSize()) {
         AndroidView(
             factory = {
